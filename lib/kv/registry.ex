42436d1b34b9f69319b8e5ca76578a1b839e66d4
zhoumh <zhoumh.vic@vsc.com>
2017-05-04 19:12:46 +0800

    Monitoring buckets

---
+++ lib/kv/registry.ex
@@ -0,0 +1,49 @@
+defmodule KV.Registry do
+  use GenServer
+
+  def start_link do
+    GenServer.start_link(__MODULE__, :ok, [])
+  end
+
+  def lookup(server, name) do
+    GenServer.call(server, {:lookup, name})
+  end
+
+  def create(server, name) do
+    GenServer.cast(server, {:create, name})
+  end
+
+  def stop(server) do
+    GenServer.stop(server)
+  end
+
+  def init(:ok) do
+    names = %{}
+    refs = %{}
+    {:ok, {names, refs}}
+  end
+  
+  def handle_call({:lookup, name}, _from, {names, _} = state) do
+    {:reply, Map.fetch(names, name), state}
+  end
+
+  def handle_cast({:create, name}, {names, refs} = state) do
+    if Map.has_key?(names, name) do
+      {:noreply, state}
+    else
+      {:ok, bucket} = KV.Bucket.start_link()
+      ref = Process.monitor(bucket)
+      refs = Map.put(refs, ref, name)
+      names = Map.put(names, name, bucket)
+      {:noreply, {names, refs}}
+    end
+  end
+
+  def handle_info({:DOWN, ref, :process, _pid, _reason}, {names, refs}) do
+    {name, refs} = Map.pop(refs, ref)
+    names = Map.delete(names, name)
+    {:noreply, {names, refs}}
+  end
+
+  def handle_info(_, state), do: {:noreply, state}
+end
---
+++ test/registry_test.exs
@@ -0,0 +1,26 @@
+defmodule KV.RegistryTest do
+  use ExUnit.Case, async: true
+  alias KV.{Bucket, Registry}
+
+  setup do
+    {:ok, registry} = Registry.start_link()
+    {:ok, registry: registry}
+  end
+
+  test "spawns buckets", %{registry: registry} do
+    assert Registry.lookup(registry, "shopping") == :error
+
+    Registry.create(registry, "shopping")
+    assert {:ok, bucket} = Registry.lookup(registry, "shopping")
+
+    Bucket.put(bucket, "milk", 1)
+    assert Bucket.get(bucket, "milk") == 1
+  end
+
+  test "removes buckets on exit", %{registry: registry} do
+    Registry.create(registry, "shopping")
+    {:ok, bucket} = Registry.lookup(registry, "shopping")
+    Agent.stop(bucket)
+    assert Registry.lookup(registry, "shopping") == :error
+  end
+end
